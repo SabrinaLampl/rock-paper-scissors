bc10d706f0e7888a004edb9e6bd6fa33c7d148a1
SabrinaLampl <lampl.sabrina@gmail.com>
2022-11-28 12:08:32 +0100

    giving computer random choice

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 console.log("Hello");
+
+
+
+let randomNumber = Math.floor(Math.random()*3);
+
+function getComputerChoice() {
+  switch (randomNumber){
+    case 0 : 
+     console.log ("Rock")
+      break;
+    case 1 : 
+     console.log ("Paper")
+      break;
+    case 2 : 
+     console.log ("Scissors")
+      break;
+  }
+}
+
+
+console.log(getComputerChoice(randomNumber))
